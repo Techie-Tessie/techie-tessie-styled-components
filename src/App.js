147a4100829fb27828775d24ea1ceaed04804d0c
Isabel G <ic.gonzalez35@hotmail.com>
2022-02-13 14:39:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import * as styles from './Button.styled';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <styles.TechieButton>
+        Techie Tessie
+      </styles.TechieButton>
+    </div>
+  );
+}
---
+++ src/Button.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const TechieButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #8c54fc;
+  border: none;
+  border-radius: 20px;
+  color: #fff;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50%;
+  padding: 20px;
+  &:hover {
+    background: #cbb3d4;
+    color: #8c54fc;
+  }
+`;
